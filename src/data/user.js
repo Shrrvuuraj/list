e2f8e6c0324f149d089dff8bccd0fe325b460c94
Shravan Rajput <shrrvvuurajput@gmail.com>
2026-07-27 01:48:38 +0530

    refactring code struture

---
+++ src/data/user.js
@@ -0,0 +1,38 @@
+export const users = [
+  {
+    id: 1,
+    name: "Shravan",
+    role: "Frontend Developer",
+    online: true,
+  },
+  {
+    id: 2,
+    name: "Rahul",
+    role: "Backend Developer",
+    online: false,
+  },
+  {
+    id: 3,
+    name: "Aman",
+    role: "UI Designer",
+    online: true,
+  },
+  {
+    id: 4,
+    name: "Priya",
+    role: "Full Stack Developer",
+    online: true,
+  },
+  {
+    id: 5,
+    name: "Neha",
+    role: "Product Designer",
+    online: false,
+  },
+  {
+    id: 6,
+    name: "Rohan",
+    role: "DevOps Engineer",
+    online: true,
+  },
+];
